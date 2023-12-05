cd28597852f3d5c9e24ff27ee8ec2a4e13806181
Kodzito11 <Kos001@edu.zealand.dk>
2023-12-05 13:57:26 +0100

    Register styling

---
+++ KingKindProjekt/Pages/OurPages/Signup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KingKindProjekt.Pages.OurPages
+{
+    public class SignupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
