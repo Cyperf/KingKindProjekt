9fe217c52fc97a62855e43f6347805cb4a9e554b
Cyperf <drageon@live.dk>
2023-12-06 14:27:23 +0100

    Newsletter FooterLayoutMix

--- KingKindProjekt/Pages/Index.cshtml.cs
+++ KingKindProjekt/Pages/Index.cshtml.cs
@@ -1,19 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KingKindProjekt.Models;
+using KingKindProjekt.Services;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml.Linq;
 
 namespace KingKindProjekt.Pages
 {
 	public class IndexModel : PageModel
 	{
 		private readonly ILogger<IndexModel> _logger;
-
+		
 		public IndexModel(ILogger<IndexModel> logger)
 		{
+			
 			_logger = logger;
 		}
 
 		public IActionResult OnGet()
-		{
+        {
 			return RedirectToPage("OurPages/ViewProducts");
 		}
 	}
