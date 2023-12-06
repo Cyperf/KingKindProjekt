bbaadc82940531e0d00f0c4bd3afe08b66d0b529
Cyperf <drageon@live.dk>
2023-12-06 12:09:03 +0100

    CheckoutButton

--- KingKindProjekt/Pages/OurPages/Checkout.cshtml.cs
+++ KingKindProjekt/Pages/OurPages/Checkout.cshtml.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using KingKindProjekt.Services;
 
 namespace KingKindProjekt.Pages.OurPages
 {
     public class CheckoutModel : PageModel
     {
+        CartService cartService { get; set; }
+        public CheckoutModel(CartService cartService)
+        {
+            this.cartService = cartService;
+        }
         public void OnGet()
         {
+            cartService._cart.Items.Clear();
         }
     }
 }
