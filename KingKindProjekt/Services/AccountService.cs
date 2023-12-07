88b31e423440abb5182957a7fe0ef919e41c0692
Jeppe August Hirsch Jeppsson <jeppe.jeppsson@live.dk>
2023-12-07 11:26:54 +0100

    Signup

--- KingKindProjekt/Services/AccountService.cs
+++ KingKindProjekt/Services/AccountService.cs
@@ -6,12 +6,25 @@ namespace KingKindProjekt.Services
     public class AccountService
     {
         Repository<Account> accounts;
+        JsonFileService<Account> jsonFileService;
 
-        public AccountService()
+        public AccountService(JsonFileService<Account> jsonFileService)
         {
+            this.jsonFileService = jsonFileService;
             accounts = new Repository<Account>();
+            var savedAccounts = jsonFileService.GetJsonItems();
+            if (savedAccounts != null)
+            {
+                foreach (var account in savedAccounts)
+                {
+                    CreateWithoutSaving(account);
+                }
+            }
+            else // dummy data
+            {
             Create(new Account("name", "email@gmail.com", "password", PrivateOrCorporation.Private, AccountType.Customer, "cvr", "AAA vej, 4000 Roskilde", "Danmark", "+45 10 02 24 90", false));
             Create(new Account("name2", "email2@gmail.com", "pass", PrivateOrCorporation.Private, AccountType.Customer, "cvr", "BBB vej, 4000 Roskilde", "Danmark", "+45 15 22 42 09", false));
+            }
         }
 
         public Account LoggedInAccount { get; set; } = null;
@@ -26,10 +39,14 @@ namespace KingKindProjekt.Services
             LoggedInAccount = temp;
             return true;
         }
-
+        public void CreateWithoutSaving(Account account)
+        {
+            accounts.Create(account.EMail, account);
+        }
         public void Create(Account account)
         {
             accounts.Create(account.EMail, account);
+            jsonFileService.SaveJsonItems(accounts.Items.Values);
         }
     }
 }
