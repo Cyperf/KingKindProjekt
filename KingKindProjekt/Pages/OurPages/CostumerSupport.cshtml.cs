c7f7eaee5a8bc8694c7a4400e5c7585cc00367ee
Cyperf <drageon@live.dk>
2023-12-12 10:07:23 +0100

    costuer

---
+++ KingKindProjekt/Pages/OurPages/CostumerSupport.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KingKindProjekt.Pages.OurPages
+{
+    public class CostumerSupportModel : PageBase
+    {
+        private readonly ILogger<CostumerSupportModel> _logger;
+
+        public CostumerSupportModel(ILogger<CostumerSupportModel> logger, Services.AccountService accountService) : base(accountService)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
